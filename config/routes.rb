cfedbc6c499e4ff3e9bf686fc21ec9a07f7bda8d
Rianna Cleary <rianna.cleary@gmail.com>
2020-02-09 15:20:19 -0500

    updated w auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :questions
   resources :user_quizzes
   resources :quizzes
-  resources :users
+  resources :users, only: [:create]
+
+
+  # CUSTOM ROUTES
+  post '/login' , to: 'users#login'
+  get '/persist', to: 'users#persist'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
